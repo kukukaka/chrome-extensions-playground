15161e7a9b3fe9b54a314108791579c53bbabb93
Kike Alonso <kikeap@gmail.com>
2020-04-16 23:42:55 +0200

    change color options

--- bgcolor-change/options.js
+++ bgcolor-change/options.js
@@ -5,7 +5,7 @@
 'use strict';
 
 let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+const kButtonColors = ['#2E3C43', '#D1D1D1', '#F15743', '#1785FB', '#01A84D' ];
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
